2571356f71f392b6a6da02fe38945339f9346a23
Ishaan Chamoli <ishaan.c.edu@gmail.com>
2025-01-01 10:42:09 +0530

    speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "./providers";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
